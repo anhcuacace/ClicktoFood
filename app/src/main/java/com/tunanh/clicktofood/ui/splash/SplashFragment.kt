6406b0bba781a46767776f8f638419ee4a879004
Anh <trantuananh291299@gmail.com>
2022-09-05 16:54:46 +0700

    clean

--- app/src/main/java/com/tunanh/clicktofood/ui/splash/SplashFragment.kt
+++ app/src/main/java/com/tunanh/clicktofood/ui/splash/SplashFragment.kt
@@ -3,10 +3,16 @@ package com.tunanh.clicktofood.ui.splash
 import android.os.Handler
 import android.os.Looper
 import android.view.View
+import android.widget.Toast
+import androidx.lifecycle.viewModelScope
 import com.tunanh.clicktofood.R
 import com.tunanh.clicktofood.databinding.FragmentSplashBinding
 import com.tunanh.clicktofood.ui.base.BaseFragment
 import com.tunanh.clicktofood.ui.main.MainActivity
+import com.tunanh.clicktofood.util.hasNetworkConnection
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class SplashFragment : BaseFragment<FragmentSplashBinding, SplashViewModel>() {
     override fun layoutRes(): Int = R.layout.fragment_splash
@@ -14,6 +20,23 @@ class SplashFragment : BaseFragment<FragmentSplashBinding, SplashViewModel>() {
     override fun viewModelClass(): Class<SplashViewModel> = SplashViewModel::class.java
 
     override fun initView() {
+        (activity as MainActivity).viewModel.isNetworkConnection.observe(this){
+            if (it) {
+                transition()
+            } else {
+                viewModel.viewModelScope.launch(Dispatchers.Main) {
+                    delay(2000)
+                    binding.animationView.visibility = View.GONE
+                    Toast.makeText(
+                        requireContext(),
+                        getString(R.string.network_fail),
+                        Toast.LENGTH_LONG
+                    )
+                        .show()
+                    (activity as MainActivity).showLoading()
+                }
+            }
+        }
 
     }
 
@@ -38,13 +61,11 @@ class SplashFragment : BaseFragment<FragmentSplashBinding, SplashViewModel>() {
     }
 
     override fun networkFail() {
-        binding.animationView.visibility = View.GONE
-        (activity as MainActivity).showLoading()
+
     }
 
     override fun successfulNetwork() {
-        (activity as MainActivity).hiddenLoading()
-        transition()
+
     }
 
 
